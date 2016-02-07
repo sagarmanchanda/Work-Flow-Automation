2f1d3fa00584e38e038d29b1b3875ea3b528d0a7
siddarthVader <siddarthkhabia@gmail.com>
2016-02-07 15:29:06 +0530

    .sql files can be connected now.

--- src/main/SkeletonClass.php
+++ src/main/SkeletonClass.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace WFA;
-
+namespace WFA\main;
+function collosus(){
+	echo "function is being called";
+}
 class SkeletonClass
 {
     /**
      * Create a new Skeleton Instance
      */
     public function __construct() {
-
+    	$this->echoPhrase("roger that!");
     }
 
     public function echoPhrase($phrase) {
---
+++ test2.php
@@ -0,0 +1,7 @@
+<?php
+require 'vendor/autoload.php';
+$test = new WFA\main\SkeletonClass();
+WFA\main\collosus();
+echo "test2";
+
+?>
