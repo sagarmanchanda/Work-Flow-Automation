f7353839efef370e337a7bab884928dafbe945ff
Saurabh Batra <saurabh.batra96@gmail.com>
2016-03-13 00:11:08 +0530

    Add unit test for Form class

---
+++ tests/FormTest.php
@@ -0,0 +1,23 @@
+<?php
+//require '../vendor/autoload.php';
+class FormTest extends PHPUnit_Framework_TestCase {
+	public function testExportInput() {
+		$form = new WFA\FormBuilder\Form();
+
+		$form->addElement('text', 'webmail', 'E-mail');
+		$form->addRule('webmail', 'required');
+		$form->addRule('webmail', 'email');
+
+		$export = $form->exportInput('webmail');
+
+		$expectedOutput = array(
+			'inputType' => 'text',
+			'name' => 'webmail',
+			'label' => 'E-mail',
+			'rule' => array('required', 'email'),
+			'defaultValue' => null,
+			);
+
+		$this->assertEquals($expectedOutput, $export);
+	}
+}
