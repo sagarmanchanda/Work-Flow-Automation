ebe1b704a602be06597907418e47425b48cdee5d
sagarmanchanda <sagarmanchanda.iitg@gmail.com>
2016-03-27 07:08:07 +0530

    Added the templates for request submit success and failure.

--- src/Templates/formTemplate.php
+++ src/Templates/formTemplate.php
@@ -4,6 +4,6 @@
 			<title>WFA Form</title>
 		</head>
 		<body>
-		<form method="POST"><label>complaint</label><input type="text" id="complaint" name="complaint" value="yo manas"><br></form>
+		<form method="POST" action="../FormBuilder/submitRequest.php" ><label>Name</label><input type="text" id="Name" name="Name" value=""><br><label>Age</label><input type="text" id="Age" name="Age" value=""><br><label>Department</label><input type="text" id="Department" name="Department" value=""><br><label>submit</label><input type="submit" id="submit" name="submit" value=""><br></form>
 		</body>
 		</html>
