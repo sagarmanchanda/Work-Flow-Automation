767934a30941d54c21d0660d4c6348c1dec59db1
sagarmanchanda <sagarmanchanda.iitg@gmail.com>
2016-04-20 01:55:14 +0530

    Updated the config file to include the Logindatabase credentials.

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+	/**
 	* It stores the hostname required to connect to MySQL.
 	*
 	* @var string
@@ -23,10 +23,36 @@
 	*/
 	$databasePassword = '';
 
+	/**
+	* It stores the hostname required to connect to MySQL.
+	*
+	* @var string
+	*/
+	$loginDatabaseHostname = 'localhost';
+
+	/**
+	* It stores the username required to connect to MySQL. The coder is expected to changes
+	* here, if required.
+	*
+	* @var string
+	*/
+	$loginDatabaseUsername = 'root';
+
+	/**
+	* It stores the password required to connect to MySQL. The coder is expected to changes
+	* here, if required.
+	*
+	* @var string
+	*/
+	$loginDatabasePassword = '';
+
 	return array(
 		'databaseHostname' => $databaseHostname,
 		'databaseUsername' => $databaseUsername,
-		'databasePassword' => $databasePassword
+		'databasePassword' => $databasePassword,
+		'loginDatabaseHostname' => $loginDatabaseHostname,
+		'loginDatabaseUsername' => $loginDatabaseUsername,
+		'loginDatabasePassword' => $loginDatabasePassword
 	);
 
 ?>
