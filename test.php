aa326ae7a155e8bee308463d59ff26b961a693f1
siddarthVader <siddharthkhabia@gmail.com>
2016-04-20 13:39:30 +0530

    test removed

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$tr = new WFA\DefineResponses\DefineResponses();
-$tr->DefineParameters("State1",["abc","d","f"]);
-$tr->addResponses("State1",['asda','22','te3n patti'],'gand fat rhi h abhi..');
-
-  ?>
