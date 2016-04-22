059fb7b116549be989eebb5709eff1113b45c4fe
sagarmanchanda <sagarmanchanda.iitg@gmail.com>
2016-04-22 16:04:08 +0530

    dashboard works fine for generation.

--- src/Templates/dashboard.php
+++ src/Templates/dashboard.php
@@ -18,6 +18,101 @@
 	</h1>
 	<br>
 	<h2><a href="../Auth/logout.php">Logout</a></h2>
-	<iframe src="formTemplate.php"></iframe>
+
+<!-- Generation Form if exists-->
+<?php
+	$stateName = $_SESSION['stateName'];
+	$config = include('../Scripts/config.php');
+	$databaseHostname = $config['databaseHostname'];
+	$databaseUsername = $config['databaseUsername'];
+	$databasePassword = $config['databasePassword'];
+	$databaseName = "requestDB";
+	$conn = new mysqli($databaseHostname, $databaseUsername, $databasePassword, $databaseName);
+	if ($conn->connect_error) {
+		die("Connection Error:".$conn->connect_error);
+	}
+
+	$sql = "SELECT * FROM AutomataStates WHERE stateName=\"".$stateName."\" AND stateType=\"generation\"";
+	$result = $conn->query($sql);
+	if($result->num_rows == 1) {
+		$templateName = $stateName."_generation.php";
+		$htmlTag = "<iframe src=\"".$templateName."\"></iframe>" ;
+		echo $htmlTag;
+	}
+	$conn->close();
+?>
+
+<!-- Generation Request Panel if exists-->
+<?php
+	$stateName = $_SESSION['stateName'];
+	$username = $_SESSION['user_name'];
+	$usernameColumn = $_SESSION['db_user_column'];
+	$config = include('../Scripts/config.php');
+	$databaseHostname = $config['databaseHostname'];
+	$databaseUsername = $config['databaseUsername'];
+	$databasePassword = $config['databasePassword'];
+	$databaseName = "requestDB";
+	$loginDatabaseHostname = $config['loginDatabaseHostname'];
+	$loginDatabaseUsername = $config['loginDatabaseUsername'];
+	$loginDatabasePassword = $config['loginDatabasePassword'];
+	$loginDatabaseName = $config['loginDatabaseName'];
+	$loginTableName = $config['loginTableName'];
+	
+	$conn = new mysqli($databaseHostname, $databaseUsername, $databasePassword, $databaseName);
+	if ($conn->connect_error) {
+		die("Connection Error:".$conn->connect_error);
+	}
+	
+	$loginconn = new \mysqli($loginDatabaseHostname, $loginDatabaseUsername, $loginDatabasePassword, $loginDatabaseName);
+	if ($loginconn->connect_error) {
+		die("Connection Error:".$loginconn->connect_error);
+	}
+
+	$sql = "SELECT * FROM AutomataStates WHERE stateName=\"".$stateName."\" AND stateType=\"generation\"";
+	$result = $conn->query($sql);
+	if($result->num_rows == 1) {
+
+		$sql = "SELECT * FROM ".$stateName."_generation WHERE inputType=\"DATABASE\"";
+		$result = $conn->query($sql);
+		$columns = [];
+		if($result->num_rows > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				array_push($columns, array('columnNameinLogin' => $row['label'], 'columnNameinMainRequest' => $row['name']) );
+			}
+		}
+		
+		$sql = "SELECT * FROM ".$loginTableName." WHERE ".$usernameColumn."=\"".$username."\"";
+		$result = $loginconn->query($sql);
+		$columnValues = [];
+		if($result->num_rows > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				foreach ($columns as $column) {
+					array_push($columnValues, $row[$column['columnNameinLogin']]);
+				}
+			}
+		}
+		
+		$sql = "SELECT * FROM RequestHandlingMain WHERE ";
+		for ($index=0; $index < count($columns)-1; $index++) { 
+			$sql .= $columns[$index]['columnNameinMainRequest']."=\"".$columnValues[$index]."\" AND ";
+		}
+		$sql .= $columns[$index]['columnNameinMainRequest']."=\"".$columnValues[$index]."\"";
+		$result = $conn->query($sql);
+		if($result->num_rows > 0){
+			echo "<div> <h3>Your request:</h3> <br>";
+			while($row = mysqli_fetch_assoc($result)) {
+				$requestID = $row['requestID'];
+				$presentState = $row['presentState'];
+				echo $requestID."  ".$presentState."<br><br>";
+				# echo "<input type=\"hidden\" name=\"requestID\" value=\"".$requestID."\">";
+			}
+			echo "</div>";
+		}
+	}
+
+	$conn->close();
+	$loginconn->close();
+?>
+
 </body>
 </html>
