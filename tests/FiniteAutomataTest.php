0b6c0bd5e50188c83f44ebb4ab45ed9252f2b4f0
Jagjot Singh <jagjot291296@gmail.com>
2016-04-26 03:35:56 +0530

    Added a new test file for FiniteAutomata.php

---
+++ tests/FiniteAutomataTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class FiniteAutomataTest extends PHPUnit_Framework_TestCase {
+	public function testexportState() {
+		$DFA = new WFA\RequestHandling\FiniteAutomata();
+
+		$DFA->addState('test01', '007', 'generation');
+		$DFA->addState('test02','008','translation');
+		$DFA->addState('test03', '009','final');
+
+		$export = $DFA->exportState('test02');
+
+		$expectedOutput = array(
+			'stateName' => 'test02',
+			'stateID' => '008',
+			'stateType' => 'translation'
+			);
+
+		$this->assertEquals($expectedOutput, $export);
+	}
+
+	public function testexportTransition() {
+		$DFA = new WFA\RequestHandling\FiniteAutomata();
+
+		$DFA->addState('test01', '007', 'generation');
+		$DFA->addState('test02','008','translation');
+		$DFA->addTransition('test_transit','test01','test02','success');
+
+		$export = $DFA->exportTransition('test_transit')
+		$expectedOutput = array(
+			'transitionName' => 'test_transit',
+			'presentState' => 'test01',
+			'nextState' => 'test02',
+			'response' => 'success'
+			);
+
+		$this->assertEquals($expectedOutput, $export);
+	}
+
+}
